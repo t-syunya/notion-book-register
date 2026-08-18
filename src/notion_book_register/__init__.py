cfe9e6f663ae3d9e1e037e9a48d5c6fd1d76be54
t-syunya <tsyunyam@gmail.com>
2026-08-18 20:09:20 +0900

    feat: NDL APIクライアントを追加

--- src/notion_book_register/__init__.py
+++ src/notion_book_register/__init__.py
@@ -2,12 +2,22 @@
 
 from notion_book_register.isbn import InvalidIsbnError, normalize_isbn13, validate_isbn13
 from notion_book_register.models import Book
+from notion_book_register.ndl_client import (
+    NdlApiError,
+    NdlClient,
+    NdlSruResponse,
+    parse_sru_response,
+)
 from notion_book_register.notion_mapping import book_to_notion_properties
 
 __all__ = [
     "Book",
     "InvalidIsbnError",
+    "NdlApiError",
+    "NdlClient",
+    "NdlSruResponse",
     "book_to_notion_properties",
     "normalize_isbn13",
+    "parse_sru_response",
     "validate_isbn13",
 ]
